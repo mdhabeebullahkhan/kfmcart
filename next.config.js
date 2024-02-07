02a209f73cb874b9ade7bed2b0f14925e3438680
mdhabeebullahkhan <appseonittechnologies@gmail.com>
2024-02-07 11:09:06 +0530

    reverting the changes back to its normal in next.config.js.

--- next.config.js
+++ next.config.js
@@ -5,36 +5,16 @@ module.exports = {
     poweredByHeader: false,
     async headers() {
         return [{
-            // source: '/(.*)',
-            // headers: createSecureHeaders({
-            //     forceHTTPSRedirect: [true, { maxAge: 63072000, includeSubDomains: true }],
-            //     referrerPolicy: "same-origin",
-            //     frameGuard: "deny",
-            //     noopen: "noopen",
-            //     nosniff: "nosniff",
-            //     xssProtection: "block-rendering",
+            source: '/(.*)',
+            headers: createSecureHeaders({
+                forceHTTPSRedirect: [true, { maxAge: 63072000, includeSubDomains: true }],
+                referrerPolicy: "same-origin",
+                frameGuard: "deny",
+                noopen: "noopen",
+                nosniff: "nosniff",
+                xssProtection: "block-rendering",
 
-            // }),
-            source: "/api/(.*)",
-            headers: [
-                // Allow for specific domains to have access or * for all
-                {
-                  key: "Access-Control-Allow-Origin",
-                  value: "*",
-                  // DOES NOT WORK
-                  // value: process.env.ALLOWED_ORIGIN,
-                },
-                // Allows for specific methods accepted
-                {
-                  key: "Access-Control-Allow-Methods",
-                  value: "GET, POST, PUT, DELETE, OPTIONS",
-                },
-                // Allows for specific headers accepted (These are a few standard ones)
-                {
-                  key: "Access-Control-Allow-Headers",
-                  value: "Content-Type, Authorization",
-                },
-              ],
+            }),
         }]
     },
 }
