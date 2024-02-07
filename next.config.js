e92186949d284aa33d0f6b1057c61afee85bd781
mdhabeebullahkhan <appseonittechnologies@gmail.com>
2024-02-07 10:55:39 +0530

    trying to fix cors issue from next.config.js.

--- next.config.js
+++ next.config.js
@@ -6,15 +6,35 @@ module.exports = {
     async headers() {
         return [{
             source: '/(.*)',
-            headers: createSecureHeaders({
-                forceHTTPSRedirect: [true, { maxAge: 63072000, includeSubDomains: true }],
-                referrerPolicy: "same-origin",
-                frameGuard: "deny",
-                noopen: "noopen",
-                nosniff: "nosniff",
-                xssProtection: "block-rendering",
+            // headers: createSecureHeaders({
+            //     forceHTTPSRedirect: [true, { maxAge: 63072000, includeSubDomains: true }],
+            //     referrerPolicy: "same-origin",
+            //     frameGuard: "deny",
+            //     noopen: "noopen",
+            //     nosniff: "nosniff",
+            //     xssProtection: "block-rendering",
 
-            }),
+            // }),
+
+            headers: [
+                // Allow for specific domains to have access or * for all
+                {
+                  key: "Access-Control-Allow-Origin",
+                  value: "*",
+                  // DOES NOT WORK
+                  // value: process.env.ALLOWED_ORIGIN,
+                },
+                // Allows for specific methods accepted
+                {
+                  key: "Access-Control-Allow-Methods",
+                  value: "GET, POST, PUT, DELETE, OPTIONS",
+                },
+                // Allows for specific headers accepted (These are a few standard ones)
+                {
+                  key: "Access-Control-Allow-Headers",
+                  value: "Content-Type, Authorization",
+                },
+              ],
         }]
     },
 }
